79628cf9ee52625c23e1e06885969bc9153295a8
antoniodcorrea <hello@antoniodiaz.me>
2022-06-06 00:51:23 +0200

    find_missing_number

--- src/problems/mod.rs
+++ src/problems/mod.rs
@@ -1,7 +1,9 @@
+mod array_consecutive_missing_number;
 mod calculate_list_max_items_difference;
 mod calculate_no_overlaps;
 mod string_capitalize;
 mod string_reverse;
+pub use array_consecutive_missing_number::find_missing_number;
 pub use calculate_list_max_items_difference::calculate_list_max_items_difference;
 pub use calculate_no_overlaps::calculate_no_overlaps;
 pub use string_capitalize::{string_capitalize_one, string_capitalize_two};
