73dfe0635097e25ec4ea74c81c87894570c959e7
antoniodcorrea <hello@antoniodiaz.me>
2022-12-07 14:14:57 +0100

    Set up basic test

--- src/problems/range_sum_bst.rs
+++ src/problems/range_sum_bst.rs
@@ -1,4 +1,25 @@
-pub fn range_sum_bst(_input: Option<()>) -> Option<()> {
+use std::cell::RefCell;
+use std::rc::Rc;
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+  pub val: i32,
+  pub left: Option<Rc<RefCell<TreeNode>>>,
+  pub right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+  #[inline]
+  pub fn new(
+    val: i32,
+    left: Option<Rc<RefCell<TreeNode>>>,
+    right: Option<Rc<RefCell<TreeNode>>>,
+  ) -> Self {
+    TreeNode { val, left, right }
+  }
+}
+
+pub fn range_sum_bst(_input: Option<Rc<RefCell<TreeNode>>>) -> Option<()> {
   None
 }
 
@@ -8,7 +29,24 @@ mod test {
 
   #[test]
   fn range_sum_first() {
-    let input: Option<()> = None;
+    // Build input
+    let node_3 = TreeNode::new(3, None, None);
+    let node_7 = TreeNode::new(7, None, None);
+    let node_5 = TreeNode::new(
+      5,
+      Some(Rc::new(RefCell::new(node_3))),
+      Some(Rc::new(RefCell::new(node_7))),
+    );
+    let node_18 = TreeNode::new(7, None, None);
+    let node_15 = TreeNode::new(5, None, Some(Rc::new(RefCell::new(node_18))));
+    let node_10 = TreeNode::new(
+      3,
+      Some(Rc::new(RefCell::new(node_5))),
+      Some(Rc::new(RefCell::new(node_15))),
+    );
+
+    // Process
+    let input: Option<Rc<RefCell<TreeNode>>> = Some(Rc::new(RefCell::new(node_10)));
     let expected_output: Option<()> = None;
     let result = range_sum_bst(input);
 
