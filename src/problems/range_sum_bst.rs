5b368b7073ed1166919b755fd2610b98705cb154
antoniodcorrea <hello@antoniodiaz.me>
2022-12-07 13:57:11 +0100

    Add basic default test

--- src/problems/range_sum_bst.rs
+++ src/problems/range_sum_bst.rs
@@ -1,1 +1,17 @@
-pub fn range_sum_bst() {}
+pub fn range_sum_bst(_input: Option<()>) -> Option<()> {
+  None
+}
+
+#[cfg(test)]
+mod test {
+  use super::*;
+
+  #[test]
+  fn range_sum_first() {
+    let input: Option<()> = None;
+    let expected_output: Option<()> = None;
+    let result = range_sum_bst(input);
+
+    assert_eq!(expected_output, result);
+  }
+}
