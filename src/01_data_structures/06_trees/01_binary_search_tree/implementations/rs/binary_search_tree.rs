19b6a6b00926f7e7b92f2c32a82764a4c86d5247
antoniodcorrea <hello@antoniodiaz.me>
2025-05-08 00:08:02 +0200

    Binary Search Tree delete rs

--- src/01_data_structures/06_trees/01_binary_search_tree/implementations/rs/binary_search_tree.rs
+++ src/01_data_structures/06_trees/01_binary_search_tree/implementations/rs/binary_search_tree.rs
@@ -46,4 +46,51 @@ impl BinarySearchTree {
       }
     }
   }
+  pub fn delete(&mut self, value: i32) -> &mut Self {
+    self.root = Self::delete_node(self.root.take(), value);
+
+    self
+  }
+
+  fn delete_node(node: Option<Box<Node>>, value: i32) -> Option<Box<Node>> {
+    match node {
+      None => None,
+      Some(mut boxed_node) => {
+        if value < boxed_node.value {
+          boxed_node.left = Self::delete_node(boxed_node.left.take(), value);
+
+          return Some(boxed_node);
+        }
+
+        if value > boxed_node.value {
+          boxed_node.right = Self::delete_node(boxed_node.right.take(), value);
+
+          return Some(boxed_node);
+        }
+
+        // Node to delete found
+        if boxed_node.left.is_none() {
+          return boxed_node.right;
+        }
+
+        if boxed_node.right.is_none() {
+          return boxed_node.left;
+        }
+
+        // Node has two children, traverse with in-order sucessor
+        let successor_value = Self::min_value_node(boxed_node.right.as_ref().unwrap()).value; // Get sucessor —deepest rights child leftmost leaf—.
+        boxed_node.value = successor_value; // Set target as sucessor value.
+        boxed_node.right = Self::delete_node(boxed_node.right, successor_value); // Remove min value from subtree.
+
+        Some(boxed_node)
+      }
+    }
+  }
+
+  fn min_value_node(mut node: &Box<Node>) -> &Box<Node> {
+    while let Some(ref left) = node.left {
+      node = left;
+    }
+    node
+  }
 }
--- src/01_data_structures/06_trees/01_binary_search_tree/implementations/rs/binary_search_tree_test.rs
+++ src/01_data_structures/06_trees/01_binary_search_tree/implementations/rs/binary_search_tree_test.rs
@@ -47,4 +47,106 @@ mod tests {
 
     assert_eq!(bst, expected_result);
   }
+
+  #[test]
+  fn deletes_node_from_empty_tree() {
+    let mut bst = BinarySearchTree::new();
+    bst.delete(1);
+
+    let expected_result = BinarySearchTree { root: None };
+
+    assert_eq!(bst, expected_result);
+  }
+
+  #[test]
+  fn deletes_node_from_tree_with_single_node() {
+    let mut bst = BinarySearchTree::new();
+    bst.insert(1);
+    bst.delete(1);
+
+    let expected_result = BinarySearchTree { root: None };
+
+    assert_eq!(bst, expected_result);
+  }
+
+  #[test]
+  fn deletes_node_with_in_order_successor_from_several_nodes() {
+    let mut bst = BinarySearchTree::new();
+    bst.insert(20).insert(5).insert(10).insert(25);
+    //     20
+    //    /  \
+    //   5    25
+    //    \
+    //    10
+
+    bst.delete(20);
+    //     25
+    //    /
+    //   5
+    //    \
+    //    10
+
+    let expected_result = BinarySearchTree {
+      root: Some(Node::new(25, Some(Node::new(5, None, Some(Node::new(10, None, None)))), None)),
+    };
+
+    assert_eq!(bst, expected_result);
+  }
+
+  #[test]
+  fn deletes_leaf_node_from_deep_tree() {
+    let mut bst = BinarySearchTree::new();
+    bst.insert(40).insert(30).insert(20).insert(10).insert(45);
+    //         40
+    //        /  \
+    //      30   45
+    //     /
+    //   20
+    //  /
+    // 10
+
+    let expected_before = BinarySearchTree {
+      root: Some(Node::new(
+        40,
+        Some(Node::new(30, Some(Node::new(20, Some(Node::new(10, None, None)), None)), None)),
+        Some(Node::new(45, None, None)),
+      )),
+    };
+
+    assert_eq!(bst, expected_before);
+
+    bst.delete(10);
+
+    let expected_after = BinarySearchTree {
+      root: Some(Node::new(
+        40,
+        Some(Node::new(30, Some(Node::new(20, None, None)), None)),
+        Some(Node::new(45, None, None)),
+      )),
+    };
+
+    assert_eq!(bst, expected_after);
+  }
+
+  #[test]
+  fn deletes_node_with_two_children() {
+    let mut bst = BinarySearchTree::new();
+    bst.insert(20).insert(10).insert(30).insert(25);
+    //     20
+    //    /  \
+    //  10    30
+    //        /
+    //      25
+
+    bst.delete(20);
+    //     25
+    //    /  \
+    //  10    30
+
+    let expected_result = BinarySearchTree {
+      root: Some(Node::new(25, Some(Node::new(10, None, None)), Some(Node::new(30, None, None)))),
+    };
+
+    assert_eq!(bst, expected_result);
+  }
 }
