7c06bf566f631ca1e873bca18ed317a051c4712c
antoniodcorrea <hello@antoniodiaz.me>
2025-05-13 12:15:54 +0200

    Graph adjacency list add_edge rs

--- src/01_data_structures/07_graph/01_adjacency_list/implementations/rs/adjacency_list.rs
+++ src/01_data_structures/07_graph/01_adjacency_list/implementations/rs/adjacency_list.rs
@@ -1,6 +1,12 @@
 use crate::hash_table::hash_table::HashTable;
 
 #[derive(Debug, PartialEq)]
+pub struct EdgeOptions {
+  pub directed: bool,
+  pub weight: Option<i32>,
+}
+
+#[derive(Debug, PartialEq)]
 pub struct Vertex {
   pub value: String,
   pub weight: Option<i32>,
@@ -24,5 +30,30 @@ impl AdjacencyList {
     self.graph.set(u, vec![]);
 
     return self;
+  }
+
+  pub fn add_edge(&mut self, u: &str, v: &str, options: Option<EdgeOptions>) -> &mut AdjacencyList {
+    if !self.graph.has(u) || !self.graph.has(v) {
+      return self;
+    }
+    let unwrapped_options = options.unwrap_or(EdgeOptions { directed: false, weight: None });
+
+    if let Some(neighbors) = self.graph.get_mut(u) {
+      neighbors.push(Vertex {
+        value: v.to_string(),
+        weight: unwrapped_options.weight,
+      });
+    }
+
+    if unwrapped_options.directed {
+      if let Some(neighbors) = self.graph.get_mut(v) {
+        neighbors.push(Vertex {
+          value: u.to_string(),
+          weight: unwrapped_options.weight,
+        });
+      }
+    }
+
+    self
   }
 }
--- src/01_data_structures/07_graph/01_adjacency_list/implementations/rs/adjacency_list_test.rs
+++ src/01_data_structures/07_graph/01_adjacency_list/implementations/rs/adjacency_list_test.rs
@@ -2,7 +2,7 @@
 mod tests {
   use crate::hash_table::hash_table::HashTable;
 
-  use super::super::adjacency_list::AdjacencyList;
+  use super::super::adjacency_list::{AdjacencyList, Vertex};
 
   #[test]
   fn adjacency_list_is_instantiated() {
@@ -30,6 +30,27 @@ mod tests {
     let expected = AdjacencyList { graph: hash_table };
     graph.add_vertex("b");
 
+    assert_eq!(graph, expected);
+  }
+
+  #[test]
+  fn adds_undirected_edge() {
+    let mut graph = AdjacencyList::new();
+    graph.add_vertex("a").add_vertex("b");
+    let _a = graph.add_edge("a", "b", None);
+
+    let mut hash_table = HashTable::new(2);
+
+    hash_table
+      .set(
+        "a",
+        vec![Vertex {
+          value: String::from("b"),
+          weight: None,
+        }],
+      )
+      .set("b", vec![]);
+    let expected = AdjacencyList { graph: hash_table };
     assert_eq!(graph, expected);
   }
 }
