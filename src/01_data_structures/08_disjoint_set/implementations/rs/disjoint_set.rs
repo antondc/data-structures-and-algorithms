3fc89820b34a89eac4f7a515a40a6c4d4463b611
antoniodcorrea <hello@antoniodiaz.me>
2025-05-14 22:39:09 +0200

    Disjoint set find rs

--- src/01_data_structures/08_disjoint_set/implementations/rs/disjoint_set.rs
+++ src/01_data_structures/08_disjoint_set/implementations/rs/disjoint_set.rs
@@ -1,14 +1,29 @@
+pub const OUT_OF_BOUNDS_ERROR: &str = "Index out of bounds";
+
 #[derive(Debug, PartialEq)]
 pub struct DisjointSet {
-  pub representatives: Vec<u32>,
-  pub ranks: Vec<u32>,
+  pub representatives: Vec<usize>,
+  pub ranks: Vec<usize>,
 }
 
 impl DisjointSet {
-  pub fn new(size: u32) -> DisjointSet {
-    let representatives: Vec<u32> = (0..size).collect();
-    let ranks: Vec<u32> = vec![0; size as usize];
+  pub fn new(size: usize) -> DisjointSet {
+    let representatives: Vec<usize> = (0..size).collect();
+    let ranks: Vec<usize> = vec![0; size as usize];
 
     DisjointSet { representatives, ranks }
+  }
+
+  pub fn find(&mut self, index: usize) -> Result<usize, &str> {
+    if index > self.representatives.len() {
+      return Err(OUT_OF_BOUNDS_ERROR);
+    }
+
+    if index != self.representatives[index] {
+      self.representatives[index] = self.find(index).unwrap();
+      return Ok(self.representatives[index]);
+    }
+
+    return Ok(self.representatives[index]);
   }
 }
--- src/01_data_structures/08_disjoint_set/implementations/rs/disjoint_set_test.rs
+++ src/01_data_structures/08_disjoint_set/implementations/rs/disjoint_set_test.rs
@@ -1,6 +1,6 @@
 #[cfg(test)]
 mod tests {
-  use super::super::disjoint_set::DisjointSet;
+  use super::super::disjoint_set::{DisjointSet, OUT_OF_BOUNDS_ERROR};
 
   #[test]
   fn disjoint_set_is_instantiated() {
@@ -11,5 +11,23 @@ mod tests {
       ranks: vec![0, 0, 0],
     };
     assert_eq!(disjoint_set, expected);
+  }
+
+  #[test]
+  fn finds_item_in_set() {
+    let mut disjoint_set = DisjointSet::new(3);
+
+    let expected = disjoint_set.find(1);
+
+    assert_eq!(expected, Ok(1));
+  }
+
+  #[test]
+  fn trows_if_item_out_of_bounds() {
+    let mut disjoint_set = DisjointSet::new(3);
+
+    let expected = disjoint_set.find(4);
+
+    assert_eq!(expected, Err(OUT_OF_BOUNDS_ERROR));
   }
 }
