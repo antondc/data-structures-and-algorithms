dd3519d838e732d03257855dc914673dda37e4cd
antoniodcorrea <hello@antoniodiaz.me>
2025-05-15 00:40:52 +0200

    Disjoint set union rs

--- src/01_data_structures/08_disjoint_set/implementations/rs/disjoint_set.rs
+++ src/01_data_structures/08_disjoint_set/implementations/rs/disjoint_set.rs
@@ -26,4 +26,37 @@ impl DisjointSet {
 
     return Ok(self.representatives[index]);
   }
+
+  pub fn union(&mut self, a: usize, b: usize) -> Result<&mut Self, &str> {
+    if a >= self.representatives.len() || b >= self.representatives.len() {
+      return Err(OUT_OF_BOUNDS_ERROR);
+    }
+
+    let representative_a = if let Ok(representative) = self.find(a) {
+      representative
+    } else {
+      return Err(OUT_OF_BOUNDS_ERROR);
+    };
+
+    let representative_b = if let Ok(representative) = self.find(b) {
+      representative
+    } else {
+      return Err(OUT_OF_BOUNDS_ERROR);
+    };
+
+    if representative_a == representative_b {
+      return Ok(self);
+    }
+
+    if self.ranks[representative_a] < self.ranks[representative_b] {
+      self.representatives[representative_a] = representative_b;
+    } else if self.ranks[representative_a] > self.ranks[representative_b] {
+      self.representatives[representative_b] = representative_a;
+    } else {
+      self.representatives[representative_b] = representative_a;
+      self.ranks[representative_a] = self.ranks[representative_a] + 1
+    }
+
+    return Ok(self);
+  }
 }
--- src/01_data_structures/08_disjoint_set/implementations/rs/disjoint_set_test.rs
+++ src/01_data_structures/08_disjoint_set/implementations/rs/disjoint_set_test.rs
@@ -30,4 +30,27 @@ mod tests {
 
     assert_eq!(expected, Err(OUT_OF_BOUNDS_ERROR));
   }
+
+  #[test]
+  fn unions_groups_of_given_representatives() {
+    let mut disjoint_set = DisjointSet::new(3);
+    let result = disjoint_set.union(1, 2).unwrap();
+
+    let expected = &DisjointSet {
+      representatives: vec![0, 1, 1],
+      ranks: vec![0, 1, 0],
+    };
+
+    assert_eq!(result, expected);
+  }
+
+  #[test]
+  fn union_throws_if_representative_out_of_bound() {
+    let mut disjoint_set = DisjointSet::new(3);
+    let result = disjoint_set.union(1, 4);
+
+    let expected = Err(OUT_OF_BOUNDS_ERROR);
+
+    assert_eq!(result, expected);
+  }
 }
