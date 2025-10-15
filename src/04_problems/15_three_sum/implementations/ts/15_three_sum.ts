c8d76327ee213882217b200365e4603d37cf0f2b
antoniodcorrea <hello@antoniodiaz.me>
2025-10-15 16:10:22 +0200

    3sum optimal

--- src/04_problems/15_three_sum/implementations/ts/15_three_sum.ts
+++ src/04_problems/15_three_sum/implementations/ts/15_three_sum.ts
@@ -33,25 +33,32 @@ export const threeSumOptimal = (nums: number[]): number[][] => {
   const results = [];
   nums.sort((a, b) => a - b);
 
+  // Fix item by item
   for (let i = 0; i < nums.length; i++) {
+    // if item same as previous, skip to avoid duplicates
     if (i > 0 && nums[i] === nums[i - 1]) {
       continue;
     }
 
+    // two pointers approach
     let left = i + 1;
     let right = nums.length - 1;
 
     while (left < right) {
       const total = nums[i] + nums[left] + nums[right];
 
+      // If total is greater than 0, we need smaller numbers, move right pointer
       if (total > 0) {
         right--;
+      // If total is smaller than 0, we need greater numbers, move left pointer
       } else if (total < 0) {
         left++;
       } else {
+        // Item found, save and move left pointer
         results.push([nums[i], nums[left], nums[right]]);
         left++;
 
+        // To avoid repetitions, if next item is the same, move left pointer again.
         while (nums[left] === nums[left - 1] && left < right) {
           left++;
         }
