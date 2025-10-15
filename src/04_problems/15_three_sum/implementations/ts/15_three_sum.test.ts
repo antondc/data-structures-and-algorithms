f3ffa823bd7c985449444eed92ed8e3ec5381015
antoniodcorrea <hello@antoniodiaz.me>
2025-10-15 16:07:13 +0200

    3sum optimal

--- src/04_problems/15_three_sum/implementations/ts/15_three_sum.test.ts
+++ src/04_problems/15_three_sum/implementations/ts/15_three_sum.test.ts
@@ -1,4 +1,5 @@
-import { threeSumSuboptimal } from './15_three_sum';
+import { threeSumOptimal, threeSumSuboptimal } from './15_three_sum';
+
 
 describe('threeSumSuboptimal', () => {
   test('Returns the correct result', () => {
@@ -75,6 +76,87 @@ describe('threeSumSuboptimal', () => {
   test('Returns the correct result', () => {
     const source = [-100000, 50000, 50000];
     const result = threeSumSuboptimal(source);
+    const expectedResult = [[-100000, 50000, 50000]];
+
+    expect(result).toEqual(expectedResult);
+  });
+});
+
+describe('threeSumOptimal', () => {
+  test('Returns the correct result', () => {
+    const source = [-1, 0, 1, 2, -1, -4];
+    const result = threeSumOptimal(source);
+    const expectedResult = [
+      [-1, -1, 2],
+      [-1, 0, 1],
+    ];
+
+    expect(result).toEqual(expectedResult);
+  });
+
+  test('Returns the correct result', () => {
+    const source = [0, 0, 0, 0];
+    const result = threeSumOptimal(source);
+
+    const expectedResult = [[0, 0, 0]];
+
+    expect(result).toEqual(expectedResult);
+  });
+
+  test('Returns the correct result', () => {
+    const source = [0, 1, 1];
+    const result = threeSumOptimal(source);
+    const expectedResult = [] as number[][];
+
+    expect(result).toEqual(expectedResult);
+  });
+
+  test('Returns the correct result', () => {
+    const source = [1, 2];
+    const result = threeSumOptimal(source);
+    const expectedResult = [] as number[][];
+
+    expect(result).toEqual(expectedResult);
+  });
+
+  test('Returns the correct result', () => {
+    const source = [1, 2, 3, 4];
+    const result = threeSumOptimal(source);
+    const expectedResult = [] as number[][];
+
+    expect(result).toEqual(expectedResult);
+  });
+
+  test('Returns the correct result', () => {
+    const source = [-5, -4, -3, -2];
+    const result = threeSumOptimal(source);
+    const expectedResult = [] as number[][];
+
+    expect(result).toEqual(expectedResult);
+  });
+
+  test('Returns the correct result', () => {
+    const source = [-2, 0, 1, 1, 2];
+    const result = threeSumOptimal(source);
+    const expectedResult = [
+      [-2, 0, 2],
+      [-2, 1, 1],
+    ];
+
+    expect(result).toEqual(expectedResult);
+  });
+
+  test('Returns the correct result', () => {
+    const source = [-2, 0, 0, 2, 2];
+    const result = threeSumOptimal(source);
+    const expectedResult = [[-2, 0, 2]];
+
+    expect(result).toEqual(expectedResult);
+  });
+
+  test('Returns the correct result', () => {
+    const source = [-100000, 50000, 50000];
+    const result = threeSumOptimal(source);
     const expectedResult = [[-100000, 50000, 50000]];
 
     expect(result).toEqual(expectedResult);
--- src/04_problems/15_three_sum/implementations/ts/15_three_sum.ts
+++ src/04_problems/15_three_sum/implementations/ts/15_three_sum.ts
@@ -27,3 +27,37 @@ export const threeSumSuboptimal = (nums: number[]): number[][] => {
 
   return results;
 };
+
+// Optimal, O(n)
+export const threeSumOptimal = (nums: number[]): number[][] => {
+  const results = [];
+  nums.sort((a, b) => a - b);
+
+  for (let i = 0; i < nums.length; i++) {
+    if (i > 0 && nums[i] === nums[i - 1]) {
+      continue;
+    }
+
+    let left = i + 1;
+    let right = nums.length - 1;
+
+    while (left < right) {
+      const total = nums[i] + nums[left] + nums[right];
+
+      if (total > 0) {
+        right--;
+      } else if (total < 0) {
+        left++;
+      } else {
+        results.push([nums[i], nums[left], nums[right]]);
+        left++;
+
+        while (nums[left] === nums[left - 1] && left < right) {
+          left++;
+        }
+      }
+    }
+  }
+
+  return results;
+};
