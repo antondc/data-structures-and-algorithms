ee40c8115925802ebec394ff7c24aebc4afd7441
antoniodcorrea <hello@antoniodiaz.me>
2026-01-15 13:00:00 +0100

    07_reverse_interegers

--- src/05_problems/07_reverse_interegers/implementations/ts/07_reverse_interegers.ts
+++ src/05_problems/07_reverse_interegers/implementations/ts/07_reverse_interegers.ts
@@ -2,15 +2,15 @@ export const reverseIntegers = (x: number): number => {
   const INT_MAX = Math.pow(2, 31);
 
   let reversed = 0;
-  let num = Math.abs(x);
+  let absoluteX = Math.abs(x);
 
-  while (num !== 0) {
-    const digit = num % 10;
-    num = Math.floor(num / 10);
+  while (absoluteX !== 0) {
+    const digit = absoluteX % 10;
+    absoluteX = Math.floor(absoluteX / 10);
 
-    // We have to check that the value we will compute, `reversed * 10 + digit`, is within bounds, i.e.,
-    // reversed * 10 + digit < INT_MAX.
-    // Solving equality, is the same as `reversed < (INT_MAX - digit) / 10`.
+    // We have to check that the value we will compute, `reversed * 10 + digit`, is within bounds,
+    // i.e.: reversed * 10 + digit < INT_MAX.
+    // Solving this equality we obtain `reversed < (INT_MAX - digit) / 10`.
     if (reversed > Math.floor(INT_MAX - digit) / 10) {
       return 0;
     }
