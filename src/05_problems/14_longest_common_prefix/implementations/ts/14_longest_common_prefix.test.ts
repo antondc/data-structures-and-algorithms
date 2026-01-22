c38f2f3f93675d71075a69a32f811850a4718641
antoniodcorrea <hello@antoniodiaz.me>
2026-01-22 12:22:20 +0100

    14_longest_common_prefix

--- src/05_problems/14_longest_common_prefix/implementations/ts/14_longest_common_prefix.test.ts
+++ src/05_problems/14_longest_common_prefix/implementations/ts/14_longest_common_prefix.test.ts
@@ -1,22 +1,48 @@
-import { longestCommonPrefix } from './14_longest_common_prefix';
+import {
+  longestCommonPrefixOptimal,
+  longestCommonPrefixSubOptimal,
+} from './14_longest_common_prefix';
 
-describe('longestCommonPrefix', () => {
+describe('longestCommonPrefixSubOptimal', () => {
   test('Returns the correct result', () => {
-    const result = longestCommonPrefix(['flower', 'flow', 'flight']);
+    const result = longestCommonPrefixSubOptimal(['flower', 'flow', 'flight']);
     const expectedResult = 'fl';
 
     expect(result).toEqual(expectedResult);
   });
 
   test('Returns the correct result', () => {
-    const result = longestCommonPrefix(['dog', 'racecar', 'car']);
+    const result = longestCommonPrefixSubOptimal(['dog', 'racecar', 'car']);
     const expectedResult = '';
 
     expect(result).toEqual(expectedResult);
   });
 
   test('Returns the correct result', () => {
-    const result = longestCommonPrefix(['interspecies', 'interstellar', 'interstate']);
+    const result = longestCommonPrefixSubOptimal(['interspecies', 'interstellar', 'interstate']);
+    const expectedResult = 'inters';
+
+    expect(result).toEqual(expectedResult);
+  });
+});
+
+describe('longestCommonPrefixOptimal', () => {
+  test('Returns the correct result', () => {
+    const result = longestCommonPrefixOptimal(['flower', 'flow', 'flight']);
+    const expectedResult = 'fl';
+
+    expect(result).toEqual(expectedResult);
+  });
+
+  test('Returns the correct result', () => {
+    const result = longestCommonPrefixOptimal(['dog', 'racecar', 'car']);
+    const expectedResult = '';
+
+    expect(result).toEqual(expectedResult);
+  });
+
+  test('Returns the correct result', () => {
+    const result = longestCommonPrefixOptimal(['interspecies', 'interstellar', 'interstate']);
     const expectedResult = 'inters';
 
     expect(result).toEqual(expectedResult);
--- src/05_problems/14_longest_common_prefix/implementations/ts/14_longest_common_prefix.ts
+++ src/05_problems/14_longest_common_prefix/implementations/ts/14_longest_common_prefix.ts
@@ -1,4 +1,31 @@
-export function longestCommonPrefix(strs: string[]): string {
+export function longestCommonPrefixSubOptimal(strs: string[]): string {
+  strs.sort();
+
+  let smallestWord = strs[0];
+  let currentChar = '';
+  let longestPrefix = [];
+
+  for (let char = 0; char < smallestWord.length; char++) {
+    for (let word = 0; word < strs.length; word++) {
+      if (word === 0) {
+        currentChar = strs[word][char];
+      }
+
+      if (strs[word][char] !== currentChar) {
+        return longestPrefix.join('');
+      }
+
+      if (word === strs.length - 1) {
+        longestPrefix.push(strs[word][char]);
+      }
+    }
+  }
+
+  return longestPrefix.join('');
+}
+
+// O(n)
+export function longestCommonPrefixOptimal(strs: string[]): string {
   strs.sort();
 
   const first = strs[0];
